229332f1f8b2c6dde5dc49b7f4ec51ffd5c8aa97
shanmukha919 <potturishanmukha919@gmail.com>
2025-12-10 10:03:56 +0530

    Fix vite base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Shannu-lakshmi-919/',   // EXACT repo name
+  base: '/Shannu-919_Lakshmi/',   // EXACT repo name
   build: {
     outDir: 'docs'
   }
